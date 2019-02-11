e13ad5c5c543925c7a1a0bc7b12ac9d74ac5cf10
Pierce Darragh <pierce.darragh@gmail.com>
2019-02-11 15:00:48 -0500

    Add EmptyBlock

---
+++ DungeonBuilder/Blocks/EmptyBlock.swift
@@ -0,0 +1,13 @@
+//
+//  EmptyBlock.swift
+//  DungeonBuilder
+//
+//  Created by Pierce Darragh on 2/11/19.
+//  Copyright © 2019 Pierce Corp. All rights reserved.
+//
+
+public class EmptyBlock: Block {
+    public var traversable: Bool = true
+
+    public required init() { }
+}
