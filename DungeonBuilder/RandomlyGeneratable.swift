2bfbe77303fc8bccaf73e2e34dcfab29e8c06ecd
Pierce Darragh <pierce.darragh@gmail.com>
2019-02-12 17:36:57 -0700

    Create RandomlyGeneratable protocol

---
+++ DungeonBuilder/RandomlyGeneratable.swift
@@ -0,0 +1,11 @@
+//
+//  RandomlyGeneratable.swift
+//  DungeonBuilder
+//
+//  Created by Pierce Darragh on 2/12/19.
+//  Copyright © 2019 Pierce Corp. All rights reserved.
+//
+
+protocol RandomlyGeneratable {
+    static func generateRandom() -> Self
+}
